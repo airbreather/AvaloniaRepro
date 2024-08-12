760ee1f20f39a3f76a001dc06a11fcd377582db3
Joe Amenta <airbreather@linux.com>
2024-08-12 08:41:19 -0400

    Repro app

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+
+namespace AvaloniaRepro;
+
+public sealed class ToolTipViewModel
+{
+    public required string Tip { get; init; }
+}
+
+public sealed class ViewLocator : IDataTemplate
+{
+    public bool Match(object? data) => data is ToolTipViewModel;
+    public Control? Build(object? param) => new ToolTipView { DataContext = param };
+}
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main(string[] args) => AppBuilder.Configure<App>()
+        .UsePlatformDetect()
+        .WithInterFont()
+        .LogToTrace()
+        .StartWithClassicDesktopLifetime(args);
+}
---
+++ ToolTipView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaRepro;
+
+public sealed partial class ToolTipView : UserControl
+{
+    public ToolTipView()
+    {
+        InitializeComponent();
+    }
+}
+
