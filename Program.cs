193fe46b516170f4b4a12eda977d66e6d428b866
Joe Amenta <airbreather@linux.com>
2024-08-13 08:23:17 -0400

    Single-file version, no repro

--- Program.cs
+++ Program.cs
@@ -1,27 +1,38 @@
-﻿using System;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Shapes;
 using Avalonia.Controls.Templates;
+using Avalonia.Data;
+using Avalonia.Media;
 
-namespace AvaloniaRepro;
+Application app = AppBuilder.Configure<Application>()
+    .UsePlatformDetect()
+    .SetupWithClassicDesktopLifetime(args)
+    .Instance!;
 
-public sealed class ToolTipViewModel
+app.DataTemplates.Add(new ViewLocator());
+Window window = new()
 {
-    public required string Tip { get; init; }
-}
+    Content = new Rectangle
+    {
+        Fill = Brushes.Red,
+        [ToolTip.TipProperty] = new { Tip = "Testing 123" },
+    },
+};
+window.Show();
+app.Run(window);
 
 public sealed class ViewLocator : IDataTemplate
 {
-    public bool Match(object? data) => data is ToolTipViewModel;
-    public Control? Build(object? param) => new ToolTipView();
-}
-
-internal static class Program
-{
-    [STAThread]
-    private static void Main(string[] args) => AppBuilder.Configure<App>()
-        .UsePlatformDetect()
-        .WithInterFont()
-        .LogToTrace()
-        .StartWithClassicDesktopLifetime(args);
+    public bool Match(object? data) => data?.GetType().GetProperty("Tip") is not null;
+    public Control Build(object? param) => new ContentPresenter
+    {
+        [!ContentPresenter.ContentProperty] = new Binding
+        {
+            Source = param,
+            Path = "Tip",
+            StringFormat = "{0} (from tooltip!)",
+        },
+    };
 }
